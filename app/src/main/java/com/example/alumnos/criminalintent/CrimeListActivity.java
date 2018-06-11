fbcdac7d12df44fe1af65d0215089e34eb66976f
JesusZamudioGil <14470435@itcampeche.edu.mx>
2018-06-11 13:59:13 -0500

    Initial commit

---
+++ app/src/main/java/com/example/alumnos/criminalintent/CrimeListActivity.java
@@ -0,0 +1,13 @@
+package com.example.alumnos.criminalintent;
+
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+    //Implementacion del metodo createFragment()
+    @Override
+    protected Fragment createFragment(){
+        return new CrimeListFragment();
+    }
+}
